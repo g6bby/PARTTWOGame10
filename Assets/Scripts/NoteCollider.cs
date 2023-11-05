1630d1738a467e31c3d043361fd8115091e41330
g6bby <nabag123@newschool.edu>
2023-11-05 00:08:33 -0400

    changed notecollider

--- Assets/Scripts/NoteCollider.cs
+++ Assets/Scripts/NoteCollider.cs
@@ -8,38 +8,46 @@ public class NoteCollider : MonoBehaviour
     public GameObject inkblot;
     public GameObject particleEffects;
     public GameObject endCollider;
+    public GameObject noteCollider;
+    public GameObject noteText;
 
-    private int triggeredNoteCount = 0;
+    private bool canPickUpNote = false;
+    //private int triggeredNoteCount = 0;
+
+
+void Start()
+{
+    inkblot.SetActive(false);
+    particleEffects.SetActive(false);
+    endCollider.SetActive(false);
+}
+
+void Update()
+    {
+        if (canPickUpNote && Input.GetKeyDown(KeyCode.E))
+        {
+            GameManager.triggeredNoteCount++;
+            Debug.Log("triggeredNoteCount: " + GameManager.triggeredNoteCount);
+            note1.SetActive(false);
+            noteCollider.SetActive(false);
+            noteText.SetActive(false);
+
+            if (GameManager.triggeredNoteCount == 5)
+            {
+                inkblot.SetActive(true);
+                particleEffects.SetActive(true);
+                endCollider.SetActive(true);
+            }
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            triggeredNoteCount++;
             note1.SetActive(true);
-
-            StartCoroutine(NextSceneDelay());
-            if (triggeredNoteCount == 5)
-            {
-
-            }
+            canPickUpNote = true;
         }
     }
 
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            note1.SetActive(false);
-        }
-    }
-
-    IEnumerator NextSceneDelay()
-    {
-        yield return new WaitForSeconds(15f);
-
-        inkblot.SetActive(true);
-        particleEffects.SetActive(true);
-        endCollider.SetActive(true);
-    }
 }
